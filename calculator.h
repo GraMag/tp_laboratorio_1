e83119742009f9725f5187c101431efafe41f728
Mag <54412818+GraMag@users.noreply.github.com>
2020-04-17 02:45:28 -0300

    TP 1 - Magali Gracia

---
+++ calculator.h
@@ -0,0 +1,13 @@
+#ifndef CALCULATOR_H_
+#define CALCULATOR_H_
+
+//int menue ();
+int addNum (char something[], char error[]);
+int sum (int num1, int num2);
+int substrac (int num1, int num2);
+int multiply (int num1, int num2);
+float divide (int num1, int num2);
+int factorial (int num);
+void message (int num1, int num2);
+
+#endif /* CALCULATOR_H_ */
